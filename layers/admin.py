91c2487b339f486377e4ad142d2f30d0835b6b17
Anna Cho <103462904+bwithchewoo@users.noreply.github.com>
2024-02-07 09:47:57 -0800

    added data_url, catalog_html, attributes, lookups, parent

--- layers/admin.py
+++ layers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
+from django import forms
 
 # Register your models here.
+
+# class ThemeAdmin(admin.ModelAdmin):
+#     list_display = ('display_name', 'name', 'order', 'primary_site', 'preview_site')
+
+#     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+#         if db_field.name == 'site':
+#             kwargs['widget'] = forms.CheckboxSelectMultiple()
+#             kwargs['widget'].attrs['style'] = 'list-style-type: none;'
+#             kwargs['widget'].can_add_related = False
+
+#         return db_field.formfield(**kwargs)
+    
+# if hasattr(settings, 'DATA_MANAGER_ADMIN'):
+#     admin.site.register(Theme, ThemeAdmin)
