e9864db16c9f3008cbe35baed8ca5190e0587200
Ryan Hodges <ryan.d.hodges@gmail.com>
2024-05-02 09:11:00 -0700

    finishing theme-level API v1 testing, beginning layer-level

--- layers/tests/test_api.py
+++ layers/tests/test_api.py
@@ -836,61 +836,70 @@ class LiveAPITests(APITestCase):
         old_themes = json.loads(mdl_theme_response.content)
         new_themes = json.loads(mls_theme_response.content)
         self.assertEqual(old_themes, new_themes)
-        # import ipdb; ipdb.set_trace()
         for idx, theme in enumerate(old_themes['themes']):
             self.assertEqual(theme, new_themes['themes'][idx])
             print("Testing theme {}: {}".format(theme['id'], theme['name']))
-            self.test_theme_children(theme['id'])
+            self.loop_through_theme_children(theme['id'])
 
-    def test_theme_children(self, theme_id):
+    def loop_through_theme_children(self, theme_id):
         dm_theme_response = requests.get('http://localhost:8002/old_manager/get_layers_for_theme/{}'.format(theme_id))
         ls_theme_response = requests.get('http://localhost:8002/data_manager/get_layers_for_theme/{}'.format(theme_id))
         old_theme_data = json.loads(dm_theme_response.content)
         new_theme_data = json.loads(ls_theme_response.content)
         self.assertEqual(old_theme_data.keys(), new_theme_data.keys())
-        if not len(old_theme_data['layers']) == len(new_theme_data['layers']):
-            import ipdb; ipdb.set_trace()
-        else:
-            self.assertEqual(len(old_theme_data['layers']), len(new_theme_data['layers']))
+        self.assertEqual(len(old_theme_data['layers']), len(new_theme_data['layers']))
         self.compare_lists(old_theme_data['layers'], new_theme_data['layers'])
+        print(" -- Testing {} layers...".format(len(new_theme_data['layers'])))
+        self.loop_though_theme_layers(new_theme_data['layers'])
 
     def compare_lists(self, old_list, new_list):
         for child in new_list:
             if not child in old_list:
-                match = None
-                try:
-                    match = next(filter(lambda record: record['id'] == child['id'], old_list))
-                except:
-                    pass
-                try:
-                    if not match or type(match) == str or type(child) == str or not match['id'] == child['id']: 
-                        print("")
-                        print("NOT FOUND: {} - {}".format(child['id'], child['name']))
-                except:
-                    import ipdb; ipdb.set_trace()
-                    print(child)
-                    print(match)
-                else:
-                    for key in child.keys():
-                        if not key in ['subLayers', 'type']:
-                            if not (child[key] == 'slider' and match[key] == 'XYZ'):
+                match = next(filter(lambda record: record['id'] == child['id'], old_list))
+                for key in child.keys():
+                    if not key in ['subLayers', 'type']:
+                        if not (
+                            (child[key] == 'slider' and match[key] == 'XYZ') or 
+                            (
+                                key == 'has_sublayers' and 
+                                child['type'] not in ['checkbox', 'radio', 'slider'] and 
+                                match['has_sublayers'] == True
+                            )
+                        ):
                                 if not child[key] == match[key]:
                                     print(child)
                                     print(match)
                                     print("Key '{}': {} ; {}".format(key, child[key], match[key]))
                                 self.assertEqual(child[key], match[key])
-                        elif key == 'type' and (child['type'] == 'slider' or child['type'] in ['checkbox',] and child['has_sublayers'] == True and match['has_sublayers'] == True):
+                    elif key == 'type' and (
+                            child['type'] == 'slider' or child['type'] in ['checkbox',] and 
+                            child['has_sublayers'] == True and match['has_sublayers'] == True
+                        ):
                             pass
+                    else:   # 'subLayers'
+                        if not (type(match[key]) == str or type(child[key])==str):
+                            self.compare_lists(match[key], child[key])
                         else:
-                            if not (type(match[key]) == str or type(child[key])==str):
-                                self.compare_lists(match[key], child[key])
-                            else:
-                                if not child[key] == match[key]:
-                                    print("Key: {}".format(key))
-                                    print("Child: {}".format(child))
-                                    print("Match: {}".format(match))
-                                    import ipdb; ipdb.set_trace()
-                                self.assertEqual(child[key], match[key])
-                    # if not child == match:
-                    #     import ipdb; ipdb.set_trace()
-                    # self.assertEqual(child, match)
+                            self.assertEqual(child[key], match[key])
+
+    def loop_though_theme_layers(self, layer_list):
+        for layer in layer_list:
+            dm_layer_response = requests.get('http://localhost:8002/old_manager/get_layer_details/{}'.format(layer['id']))
+            ls_layer_response = requests.get('http://localhost:8002/data_manager/get_layer_details/{}'.format(layer['id']))
+            old_layer_data = json.loads(dm_layer_response.content)
+            new_layer_data = json.loads(ls_layer_response.content)
+            self.assertEqual(old_layer_data.keys(), new_layer_data.keys())
+            # self.assertEqual(len(old_theme_data['layers']), len(new_theme_data['layers']))
+            self.compare_layers(old_layer_data, new_layer_data)
+
+    def compare_layers(self, old_layer, new_layer):
+        for key in old_layer.keys():
+            if not old_layer[key] == new_layer[key]:
+                print("=================")
+                print("ID: {}".format(old_layer['id']))
+                print("Name: {}".format(old_layer['name']))
+                print("KEY: {}".format(key))
+                print("OLD: {}".format(old_layer[key]))
+                print("NEW: {}".format(new_layer[key]))
+                import ipdb; ipdb.set_trace()
+                print("=================")
