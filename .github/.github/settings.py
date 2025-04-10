40919f85a81e42d01f96a91ec06dc81cdb593afc
David Pollard <pollardld@gmail.com>
2025-04-09 18:31:42 -0700

    Update Django version to 3.10.12 for Github workflow

--- .github/.github/settings.py
+++ .github/.github/settings.py
@@ -22,8 +22,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Add the port your React app runs on
 ]
+
 CORS_ORIGIN_WHITELIST = [
-
-     '<http://localhost:3000>',  # The default port for create-react-app
-
+    'http://localhost:3000',  # The default port for create-react-app
 ]
